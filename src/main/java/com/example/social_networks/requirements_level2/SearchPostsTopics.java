52d81470d7f6248c12c09d3a74f1689dac8c1584
Kerlos Melad Fakhry Atallah Hanna <kerloshanna03@gmail.com>
2025-12-21 21:50:28 +0200

    Add files via upload

---
+++ src/main/java/com/example/social_networks/requirements_level2/SearchPostsTopics.java
@@ -0,0 +1,7 @@
+package com.example.social_networks.requirements_level2;
+
+public class SearchPostsTopics {
+    public static void searchPostsTopics() {
+
+    }
+}
---
+++ src/main/java/com/example/social_networks/requirements_level2/SearchPostsWords.java
@@ -0,0 +1,7 @@
+package com.example.social_networks.requirements_level2;
+
+public class SearchPostsWords {
+    public static void searchPostsWords() {
+
+    }
+}
