cf1c4ee6342d17d5d0fb3730b9472170d5cc9049
dovedove2 <david_200923@yahoo.com>
2025-12-24 14:26:13 +0200

    Refactor MostInfluencer to use graph indegree
    Refactored findMostInfluencer method to use graph indegree instead of XML parsing.

--- src/main/java/com/example/social_networks/requirements_level2/MostInfluencer.java
+++ src/main/java/com/example/social_networks/requirements_level2/MostInfluencer.java
@@ -1,63 +1,33 @@
 package com.example.social_networks.requirements_level2;
 
-import com.example.social_networks.requirements_level1.parsing_xml.*;
-import java.util.Vector;
-
 public class MostInfluencer {
     /**
-     * Parses through the XML file (through parsing_xml class) 
-     * then returns the ID of the user with the most followers
+     * Finds the user with the most followers using graph indegree.
+     * returns the ID of that user
      * note: does not handle ties
      */
-    public static int findMostInfluencer(String xml) {
+    public static int findMostInfluencer(Graph g) {
 
-        Vector<Tag> tags = Parsing_XML.parse(xml);
+        int n = g.adj.size();
+        int[] indegree = new int[n];
 
-        Vector<Integer> userIds = new Vector<>();
-        Vector<Integer> followerCounts = new Vector<>();
-
-        boolean insideUser = false;
-        int currentUserId = -1;
-        int currentFollowerCount = 0;
-
-        for (Tag tag : tags) {
-
-            // Start of a user
-            if (tag.isOpening && tag.name.equals("user")) {
-                insideUser = true;
-                currentUserId = -1;
-                currentFollowerCount = 0;
-            }
-
-            // Save user ID
-            else if (insideUser && tag.name.equals("id") && tag.isOpening && currentUserId == -1) {
-                currentUserId = Integer.parseInt(tag.innerText);
-            }
-
-            // Add 1 to follower count for each follower tag
-            else if (insideUser && tag.name.equals("follower") && tag.isOpening) {
-                currentFollowerCount++;
-            }
-
-            // End of user tag, save data
-            else if (tag.isClosing && tag.name.equals("user")) {
-                insideUser = false;
-
-                userIds.add(currentUserId);
-                followerCounts.add(currentFollowerCount);
+        // Calculate indegree of each node
+        for (int u = 0; u < n; u++) {
+            for (int v : g.adj.get(u)) {
+                indegree[v]++;
             }
         }
 
-        int maxFollowers = -1;
+        int maxIndegree = -1;
         int influencerId = -1;
-
-        for (int i = 0; i < userIds.size(); i++) {
-            if (followerCounts.get(i) > maxFollowers) {
-                maxFollowers = followerCounts.get(i);
-                influencerId = userIds.get(i);
+        
+        // Find node with highest indegree
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] > maxIndegree) {
+                maxIndegree = indegree[i];
+                influencerId = i;
             }
         }
-
         return influencerId;
     }
 }
